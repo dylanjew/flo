f781d3f3ef6049d8539cc2e724556a3ade0ebd56
Dylan Jew <dylanjew1@gmail.com>
2016-04-04 15:44:32 -0700

    fix hmr for build

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -17,11 +17,11 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     // we are building the test environment in ./spec-bundle.js
-    files: [ { pattern: 'spec-bundle.js', watched: false } ],
+    files: [ { pattern: './config/spec-bundle.js', watched: false } ],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { 'spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+    preprocessors: { './config/spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
 
     // Webpack Config at ./webpack.test.config.js
     webpack: testWebpackConfig,
